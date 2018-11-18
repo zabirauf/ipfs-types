0c765300eaf1beec4989ba94ee30acdaa472d583
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-11-18 15:52:21 +0800

    added add, get, async code in example

--- examples/index.ts
+++ examples/index.ts
@@ -1,10 +1,24 @@
 import * as IPFS from "typestub-ipfs";
-let node = new IPFS({})
-console.log('waiting node')
-node.on('ready',()=>{
-    console.log('node is ready')
-    node.stop(()=>console.log('node is closed'))
-})
-node.on('error',(error:any)=>{
-    console.error(error.message)
-})
+
+(async () => {
+    let node = new IPFS();
+    node.on('error', error => console.error(error.message))
+
+    console.log('waiting node...');
+    await new Promise((resolve, reject) => node.on('ready', resolve));
+    console.log('node is ready.');
+
+    console.log('adding file...');
+    // FIXME first argument cannot be string?
+    let files = await node.files.add(new Buffer('hello'));
+    console.log('added file:', {files});
+
+    let hash = files[0].hash;
+    console.log('getting file...');
+    let res = await node.files.get(hash);
+    console.log('got file:', {res});
+
+    console.log('closing node...');
+    await new Promise((resolve, reject) => node.stop(() => resolve))
+    console.log('closed node.');
+})();
